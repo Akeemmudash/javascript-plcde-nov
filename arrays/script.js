588d5ff6deb65b74133d2beacb5fad3c640b187c
Akeem Mudashiru <akeemmudash@gmail.com>
2026-03-01 16:34:29 +0100

    arrays

---
+++ arrays/script.js
@@ -0,0 +1,14 @@
+// const myArray = ["Hello", "New", true, false, 2, { identity: "TI employee" }];
+// console.log("before", myArray);
+// myArray[5] = "last element";
+// console.log("after", myArray);
+// myArray[6] = "New last element";
+// console.log("new update", myArray);
+// myArray[8] = "New Element added at position 9";
+// console.log("new update", myArray);
+// console.log(myArray[10]);
+
+const myArray = ["h", "e", "l", "l", "o"];
+//get the last index of the array
+// add an element w to the array
+// change the first element of the array to have the value of 'm'

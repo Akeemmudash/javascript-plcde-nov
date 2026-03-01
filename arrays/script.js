29eb2e47aa61838ba66c143084720f9a59a39abf
Akeem Mudashiru <akeemmudash@gmail.com>
2026-03-01 17:03:27 +0100

    array methods

--- arrays/script.js
+++ arrays/script.js
@@ -8,7 +8,24 @@
 // console.log("new update", myArray);
 // console.log(myArray[10]);
 
+function logUpdatedArr() {
+  console.log("myArray", myArray);
+}
 const myArray = ["h", "e", "l", "l", "o"];
 //get the last index of the array
-// add an element w to the end of the array
+const lastIndex = myArray.length - 1;
+myArray.push("w");
+logUpdatedArr();
+myArray[0] = "m";
+
+const lastElement = myArray.pop();
+console.log("lastElement", lastElement);
+
+myArray.unshift("p");
+logUpdatedArr();
+const removedElement = myArray.shift();
+console.log("removedElement", removedElement);
+logUpdatedArr();
+
+// add an element 'w' to the end of the array
 // change the first element of the array to have the value of 'm'
